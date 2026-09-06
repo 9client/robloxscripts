148cc5622b1138a88157b4fff6b97cf57f0023fd
jade <jade@MANGO>
2026-09-06 03:37:36 -0700

    AUUGHJJIGHj

--- ban script.lua
+++ ban script.lua
@@ -1,9 +1,16 @@
-local user = 1zqf
-if game.Players:FindFirstChild(user) then
-    game.Players[user]:Kick()
+local msg = [[get out of here stinky!]]
+local stinky = {"1zqf"}
+
+for _,user in pairs(stinky) do
+    if game.Players:FindFirstChild(user) then
+        game.Players[user]:Kick(msg)
+    end
 end
+
 game.Players.PlayerAdded:connect(function(plr)
-    if plr.Name == user then
-        plr:Kick()
+    for _,user in pairs(stinky) do
+        if plr.Name == user then
+            plr:Kick(msg)
+        end
     end
 end)
